1fc751a43704991a305176716c4113bc8b26b990
ctrlprogrammer <sebastianto1999@gmail.com>
2024-11-15 04:53:51 -0500

    Amazon lex integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,29 @@
 import Express from "express";
+import Cors from "cors";
+import { LexRoutes } from "./lex";
+import Dotenv from "dotenv";
 
 class APICore {
   private app: Express.Application = Express();
   private port: string = "3000";
+  private routes: any[] = [];
+
+  constructor() {
+    Dotenv.config();
+  }
 
   private intializeEnv() {
     this.port = process.env.PORT || this.port;
   }
 
-  private initializeMiddlewares() {}
+  private initializeMiddlewares() {
+    this.app.use(Cors());
+    this.app.use(Express.json());
+  }
 
-  private initializeRoutes() {}
+  private initializeRoutes() {
+    this.routes = [new LexRoutes(this.app)];
+  }
 
   public initServer() {
     this.intializeEnv();
@@ -18,7 +31,9 @@ class APICore {
     this.initializeRoutes();
 
     this.app.listen(this.port, () => {
-      console.log(`Example app listening on port ${this.port}`);
+      console.log(`Listening on port ${this.port}`);
     });
   }
 }
+
+new APICore().initServer();
--- server/src/lex.ts
+++ server/src/lex.ts
@@ -1,1 +1,61 @@
-class LexLibrary {}
+import Express from "express";
+import { LexRuntimeV2Client, RecognizeTextCommand, RecognizeTextCommandInput } from "@aws-sdk/client-lex-runtime-v2";
+import { fromEnv } from "@aws-sdk/credential-providers";
+
+const BOT_NAME = "StoreInventoryManager";
+
+export class Router {
+  protected app: Express.Application;
+
+  constructor(app: Express.Application) {
+    this.app = app;
+  }
+
+  public validate() {
+    return this.app != null;
+  }
+}
+
+export class LexRoutes extends Router {
+  private lexClient: LexRuntimeV2Client;
+
+  constructor(app: Express.Application) {
+    super(app);
+
+    this.lexClient = new LexRuntimeV2Client({
+      region: "us-east-1",
+      credentials: fromEnv(),
+    });
+
+    this.createRoutes();
+  }
+
+  private createRoutes() {
+    this.app.get("/state", (req, res) => this.getState(req, res));
+    this.app.get("/test", (req, res) => this.receiveMessage(req, res));
+  }
+
+  private getState(_: Express.Request, res: Express.Response) {
+    res.json({
+      error: false,
+      data: true,
+    });
+  }
+
+  private async receiveMessage(req: Express.Request, res: Express.Response) {
+    const lexParams: RecognizeTextCommandInput = {
+      botId: "2098HGEBIJ",
+      botAliasId: "3LVER1CIMH",
+      text: "Hola",
+      localeId: "es_419",
+      sessionId: "test_session_1",
+    };
+
+    try {
+      const data = await this.lexClient.send(new RecognizeTextCommand(lexParams));
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
